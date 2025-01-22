a9adf9bdd21b72f3215125b49c4b8138dc8eb5c4
sebastian carrero <mugssebastian@gmail.com>
2025-01-21 22:34:28 -0500

    lua

---
+++ lua/dia1.lua
@@ -0,0 +1,12 @@
+-- Declarar variables
+local nombre = "Samuel Largo"
+local edad = 10
+
+-- Usar un condicional
+if edad > 18 then
+    print("Hola " .. nombre .. ", eres mayor de edad.")
+else
+    print("Hola " .. nombre .. ", eres menor de edad.")
+end
+
+
---
+++ lua/hola.lua
@@ -0,0 +1,2 @@
+print('Hola samuel este es mi primer programa en lua')
+print(111111 + 22222222)
