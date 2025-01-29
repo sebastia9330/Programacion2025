da7a56a268755a08055ebe465b7025460f66a0d7
sebastian carrero <mugssebastian@gmail.com>
2025-01-29 14:39:31 -0500

    operadores

---
+++ miduPython/operadores.py
@@ -0,0 +1,48 @@
+### Operadores Aritmeticos###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(11 % 2)
+print(11 // 2)
+print(11 ** 2)
+print(11 ** 2 + 3 - 7 / 1 // 4)
+
+print("Hola " + "Python")
+print("Hola " + str(5) )
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+
+### Operadores Comparativos###
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Python") #Orden alfabetico por ASCII
+print(len("Hola") >= len("Python")) #Cuenta Caracteres
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+
+### Operadores Logicos###
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+#print(3 > 4 not "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" < "Python")
+print(3 < 4 and "Hola" > "Python")
+print(3 < 4 or "Hola" > "Python")
+print(not(3 > 4))
+
