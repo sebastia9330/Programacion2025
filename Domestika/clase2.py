b3a14376a585957df85a26c9ce8bc04f1e8fcb36
sebastian carrero <mugssebastian@gmail.com>
2025-02-11 16:54:25 -0500

    curso domestika

---
+++ Domestika/clase2.py
@@ -0,0 +1,12 @@
+nombre = "Sebastian" #Datos tipo texto
+apellido = "Carrero" #Datos tipo texto
+edad = 20 #Datos tipo numericos
+
+nombre = "Samuel"
+
+print(nombre,apellido, edad)
+
+
+edad = edad + 1
+
+print(nombre,apellido, edad)
---
+++ miduPython/listas.py
@@ -0,0 +1,20 @@
+###### Listas ######
+
+miLista = []
+
+print(len(miLista))
+
+miLista = [35,24,62,17,52,45,21]
+
+print(miLista)
+print(len(miLista))
+
+datosLista = [30, 1.70, "Sebastián", "Carrero"]
+print(type(datosLista))
+print(datosLista)
+print(datosLista[0])
+print(datosLista[-1])
+print(datosLista[1])
+print(datosLista[-3])
+#print(datosLista[4]) IndexError: list index out of range
+#print(datosLista[-5]) IndexError: list index out of range
