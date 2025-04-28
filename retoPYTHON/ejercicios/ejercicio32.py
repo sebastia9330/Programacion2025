f83f9de352f675b2b477b72ef7ed92b84af9477a
sebastian carrero <mugssebastian@gmail.com>
2025-04-27 19:42:36 -0500

    ejercicio 32

---
+++ retoPYTHON/ejercicios/ejercicio32.py
@@ -0,0 +1,11 @@
+#Ejercicio 32
+#Escribe un programa que sume todos los números del 1 al 100 usando un bucle while.
+
+numero = 0
+result = 0
+
+while(numero != 100):
+    numero +=1
+    result += numero
+
+print(result)
