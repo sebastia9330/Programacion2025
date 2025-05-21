e1aa9ac7cf087ef73a499cb5dea1bd1c75ace2a2
sebastian carrero <mugssebastian@gmail.com>
2025-05-21 10:51:28 -0500

    ejercicio 60

---
+++ retoPYTHON/ejercicios/ejercicio60.py
@@ -0,0 +1,14 @@
+#Ejercicio 60
+#Elimina los elementos duplicados de una lista ingresada por el usuario y muestra la nueva lista sin repeticiones.
+
+
+nombres = ["Ana", "Luis", "Carlos", "María", "Pedro", "Laura", "José", "Elena", "Miguel", "Sofía",
+        "Andrés", "Lucía", "Javier", "Camila", "Diego", "Valentina", "Raúl", "Carla", "Daniel", "Fernanda",
+        "Marco", "Isabela", "Tomás", "Paula", "Hugo", "Gabriela", "Felipe", "Rosa", "Iván", "Clara", "aliria",
+        "Ana", "Luis", "Carlos", "María", "Pedro", "Laura", "José", "Elena", "Miguel", "Sofía",
+        "Andrés", "Lucía", "Javier", "Camila", "Diego", "Valentina", "Raúl", "Carla", "Daniel", "Fernanda",
+        "Marco", "Isabela", "Tomás", "Paula", "Hugo", "Gabriela", "Felipe", "Rosa", "Iván", "Clara", "aliria"]
+
+nombresUnicos = list(set(nombres))
+
+print(nombresUnicos)
